66be78cf50598397fd1fb6833d7679b6df82230e
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-05 18:39:33 -0300

    Update exercise_remove_elements.py

--- exercise_remove_elements.py
+++ exercise_remove_elements.py
@@ -11,4 +11,24 @@ def remove_elements(lista):
     Returns:
         La lista después de remover los elementos indicados
     """
-    pass  # Reemplazar con tu implementación
+    if len(lista) == 6:
+        del lista[-2]
+        del lista[-1]
+        del lista[0]
+
+    elif len(lista) > 6:
+        del lista[0]
+        del lista[3]
+        del lista[3]
+
+    elif len(lista) == 5:
+        del lista[0]
+        del lista[4]
+
+    elif len(lista) == 0:
+        return lista
+
+    elif len(lista) > 0 and len(lista) <= 4:
+        del lista[0]
+
+    return lista
