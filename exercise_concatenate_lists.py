33a5092a23f6b08448f1efdfeb534798ac463d88
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-05 18:23:35 -0300

    Update exercise_concatenate_lists.py

--- exercise_concatenate_lists.py
+++ exercise_concatenate_lists.py
@@ -11,4 +11,4 @@ def concatenate_lists(lista1, lista2):
     Returns:
         Una nueva lista con todos los elementos de lista1 seguidos de lista2
     """
-    pass  # Reemplazar con tu implementación
+    return lista1 + lista2
