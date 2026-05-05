fb6f6627ebca5c6532f4c0cae5e7a85b260f0fe6
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-05 18:26:01 -0300

    Update exercise_find_min.py

--- exercise_find_min.py
+++ exercise_find_min.py
@@ -11,4 +11,7 @@ def find_min(lista):
     Returns:
         El valor mínimo de la lista o None si está vacía
     """
-    pass  # Reemplazar con tu implementación
+    if len(lista) == 0:
+        return None
+    elif len(lista) >= 1:
+        return min(lista)
