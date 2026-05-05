e9a4b6296406ac44612ef44f109e7b2de14e368a
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-05 18:38:12 -0300

    Update exercise_list_of_lists.py

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -13,4 +13,7 @@ def list_of_lists(lista_de_listas):
     Returns:
         La lista de listas modificada según las reglas
     """
-    pass  # Reemplazar con tu implementación
+    lista_de_listas [0] = lista_de_listas[0] [:2]
+    lista_de_listas[1] = lista_de_listas[1] [1:4]
+    lista_de_listas[2] = lista_de_listas[2] [-2:]
+    return lista_de_listas
