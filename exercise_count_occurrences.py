0f99bbf0b9c0977953d4025f542507247072a312
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-05-05 18:24:30 -0300

    Update exercise_count_occurrences.py

--- exercise_count_occurrences.py
+++ exercise_count_occurrences.py
@@ -11,4 +11,4 @@ def count_occurrences(lista, elemento):
     Returns:
         Un entero con la cantidad de veces que aparece el elemento
     """
-    pass  # Reemplazar con tu implementación
+    return lista.count(elemento)
